7daec7e5c45b9a8df4d0fe052f34274040d54396
Krupka <krupka.ua@gmail.com>
2021-11-06 21:15:45 +0200

    web_projects_with_vanilla_js - save data to local storage

--- js/web_projects_with_vanilla_js/movie-seat-booking/js/script.js
+++ js/web_projects_with_vanilla_js/movie-seat-booking/js/script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.container');
-const seat = document.querySelectorAll('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
@@ -7,10 +7,27 @@ const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
 console.log('ticketPrice', ticketPrice);
 
+// SAVE SELECTED MOVIE INDEX AND PRICE
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // UPDATE TOTAL AND COUNT
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     console.log('selectedSeats', selectedSeats);
+
+    // Copy selected seats into arr
+    // Map through array
+    // return a new array indexes
+
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+    console.log('seatsIndex', seatsIndex);
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+
 
     const selectedSeatsCount = selectedSeats.length;
     console.log('selectedSeatsCount', selectedSeatsCount);
@@ -21,7 +38,9 @@ function updateSelectedCount() {
 
 // MOVIE SELECT EVENT
 movieSelect.addEventListener('change', event => {
-    ticketPrice = event.target.value;
+    ticketPrice = +event.target.value;
+    setMovieData(event.target.selectedIndex, event.target.value);
+    console.log('selectedIndex & price', event.target.selectedIndex, event.target.value);
     updateSelectedCount();
 })
 
