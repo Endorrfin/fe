a82fbbeb0480f3e952d749e37075db057ad5df11
Krupka <krupka.ua@gmail.com>
2021-11-07 21:49:56 +0200

    complete-js-course - 012 Data Types

--- js/complete-js-course/01-Fundamentals-Part-1/starter/script.js
+++ js/complete-js-course/01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,8 @@
 let js = 'amazing';
 // if(js === 'amazing') alert('JavaScript is FUN!');
 
+
+/*
 console.log(40 + 8 + 23 + -10);
 
 console.log('Jonas');
@@ -12,6 +14,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable name conventions
 let jonas_matilda = "JM";
 let $function = 27;
 
@@ -25,6 +28,82 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
+
+// THE 7 PRIMITIVE DATA TYPES
+
+// 1. Number
+let age = 23;
+// 2. String
+let firstName = 'Jonas';
+
+// 3. Boolean
+let fullAge = true;
+
+// 4. Undefined
+let children;
+
+// 5. Null: Also means 'empty value'
+
+// 6. Symbol (ES2015): Value that is unique and cannot be changed [Not useful for now]
+
+// 7. BigInt (ES2020): Larger integers than the Number type can hold
+
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof  true);
+console.log(typeof  javascriptIsFun);
+console.log(typeof   23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year); // undefined
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
