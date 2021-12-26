7b28fc277c3b25e0eba8b7cb26b2994c1bfdb2f1
Krupka <krupka.ua@gmail.com>
2021-12-26 13:44:40 +0200

    complete-js-course - 18 Taking Decisions_ if _ else Statements

--- js/complete-js-course/01-Fundamentals-Part-1/script.js
+++ js/complete-js-course/01-Fundamentals-Part-1/script.js
@@ -192,6 +192,28 @@ GOOD LUCK 😀
 // lines`);
 
 
+// ============ TAKING DECISIONS IF ELSE STATEMENT ============
+
+// const age = 15;
+//
+// if (age >= 18) {
+//     console.log('Sarah can start driving license ');
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+// }
+//
+//
+// const birthYear = 1998;
+//
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+//
+// console.log(century);
 
 
 
