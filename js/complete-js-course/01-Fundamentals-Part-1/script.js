956f4a97fabbd97d3fcded634c2e210e47b243e8
Krupka <krupka.ua@gmail.com>
2021-12-26 15:19:02 +0200

    complete-js-course - 21 Truthy and Falsy Values

--- js/complete-js-course/01-Fundamentals-Part-1/script.js
+++ js/complete-js-course/01-Fundamentals-Part-1/script.js
@@ -280,4 +280,37 @@ GOOD LUCK 😀
 // console.log('10' - '4' - '3' - 2 + '5'); // 15
 
 
+// ============ 21 TRUTHY & FALSY VALUES ============
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean('')); // false
+console.log(Boolean('Jonas')); // true
+console.log(Boolean({})); // true
+console.log(Boolean([])); // true
+
+const money = 10;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log("You should get a job!");
+}
+
+let height;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED');
+}
+
+
+
+
+
+
+
+
+
 
