67bf2ee18c5d8bf96f871677b652816b1ef5ae74
Krupka <krupka.ua@gmail.com>
2021-12-26 20:35:31 +0200

    complete-js-course - 24 Logical Operators

--- js/complete-js-course/01-Fundamentals-Part-1/script.js
+++ js/complete-js-course/01-Fundamentals-Part-1/script.js
@@ -332,6 +332,36 @@ GOOD LUCK 😀
 // }
 
 
+// ============ 23 BOOLEAN LOGIC ============
+// Visual theory
+
+// ============ 24 LOGICAL OPERATORS ============
+
+// const hasDriversLicense = true; // A
+// const hasGoodVision = false; // B
+//
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if (shouldDrive) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+// const isTired = true;
+// console.log(hasDriversLicense || hasGoodVision || isTired);
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
 
 
 
