c634a3c9d44225b791c2ab7d906fc0905b64c972
Krupka <krupka.ua@gmail.com>
2021-12-26 13:29:11 +0200

    complete-js-course - 17 Strings and Template Literals

--- js/complete-js-course/01-Fundamentals-Part-1/script.js
+++ js/complete-js-course/01-Fundamentals-Part-1/script.js
@@ -168,7 +168,28 @@ GOOD LUCK 😀
 // console.log('BMIMark', BMIMark.toFixed(1), 'BMIJohn', BMIJohn.toFixed(1), 'markHigherBMI', markHigherBMI);
 
 
+// ============ STRINGS & TEMPLATE LITERALS ============
 
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + 'years old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew)
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple
+lines`);
 
 
 
