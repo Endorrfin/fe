694d96e66653fc048b41cb01ac6d9f0886a2e422
Krupka <krupka.ua@gmail.com>
2021-12-27 12:00:51 +0200

    complete-js-course - 32 Activating Strict Mode

---
+++ js/complete-js-course/02-Fundamentals-Part-2/script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// ============ 32 ACTIVATING STRICT MODE ============
+
+// let hasDriversLicense = false;
+// const passTest = true;
+//
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio'; // interface is not allowed as identifier in strict mode
+// const private = 534 // private is not allowed as identifier in strict mode
+
+
+
+
+
+
+
+
+
+
