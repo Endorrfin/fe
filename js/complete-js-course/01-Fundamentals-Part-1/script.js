b6fcaca32c78bef5bc33cb74f91f0666fe071281
Krupka <krupka.ua@gmail.com>
2021-12-27 10:59:50 +0200

    complete-js-course - 28 The Conditional (Ternary) Operator

--- js/complete-js-course/01-Fundamentals-Part-1/script.js
+++ js/complete-js-course/01-Fundamentals-Part-1/script.js
@@ -456,8 +456,27 @@ GOOD LUCK 😀
 // }
 
 
+// ============ 27 STATEMENT & EXPRESSIONS ============
+// theory
 
 
+// ============ 28 THE CONDITIONAL (TERNARY) OPERATOR ============
+
+// const age = 17;
+// age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+// const drink = age >= 18 ? 'wine' : 'water';
+// console.log(drink);
+
+// let drink2;
+// if (age >= 18) {
+//     drink2 = 'wine';
+// } else {
+//     drink2 = 'water';
+// }
+// console.log(drink2);
+
+// console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
 
 
 
