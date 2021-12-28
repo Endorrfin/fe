7e85b976a2c40524b2c4fba9d8b622abd48379e4
Krupka <krupka.ua@gmail.com>
2021-12-28 12:07:07 +0200

    complete-js-course - 33 Functions

--- js/complete-js-course/02-Fundamentals-Part-2/script.js
+++ js/complete-js-course/02-Fundamentals-Part-2/script.js
@@ -12,6 +12,35 @@
 // const private = 534 // private is not allowed as identifier in strict mode
 
 
+// ============ FUNCTIONS  ============
+
+// function logger() {
+//     console.log('My name is Jonas');
+// }
+
+// calling / running / invoking function
+// logger();
+// logger();
+// logger();
+
+
+// function fruitProcessor(apples, oranges) {
+    // console.log(apples, oranges);
+//     const juice =  `Juice with ${apples} apples and ${oranges} oranges.`
+//     return juice;
+// }
+
+// fruitProcessor (5, 0);
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+// const appleOrangeJuice = fruitProcessor(2,4);
+// console.log(appleOrangeJuice);
+
+// const num = Number('23');
+
 
 
 
