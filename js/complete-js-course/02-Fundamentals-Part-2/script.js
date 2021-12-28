e3ef45afc1369419af6199662b18e68ead7ed1f0
Krupka <krupka.ua@gmail.com>
2021-12-28 15:33:55 +0200

    complete-js-course - 40 Basic Array Operations (Methods)

--- js/complete-js-course/02-Fundamentals-Part-2/script.js
+++ js/complete-js-course/02-Fundamentals-Part-2/script.js
@@ -226,14 +226,45 @@ GOOD LUCK 😀
 // console.log('ages', ages);
 
 
+// ============ 40 Basic Array Operations (Methods) ============
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends); // ['Michael', 'Steven', 'Peter', 'Jay']
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends); // ['John', 'Michael', 'Steven', 'Peter', 'Jay']
+
+// Remove elements
+friends.pop(); // Last
+
+const popped = friends.pop();
+console.log(popped); // Peter
+console.log(friends); // ['John', 'Michael', 'Steven']
+
+friends.shift(); // First
+console.log(friends); // ['Michael', 'Steven']
 
 
+console.log(friends.indexOf('Steven')); // 1
+console.log(friends.indexOf('Bob')); // -1
 
 
+friends.push(23);
+console.log(friends.includes('Steven')); // true
+console.log(friends.includes('Bob')); // false
+console.log(friends.includes(23)); // false
 
+if (friends.includes('Peter')) {
+    console.log('You have a friend called Peter');
+}
 
-
-
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven');
+}
 
 
 
