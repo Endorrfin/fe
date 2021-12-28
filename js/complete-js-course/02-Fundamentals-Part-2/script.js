abd85506c159b9b82e3555be4f84f20cc89780a6
Krupka <krupka.ua@gmail.com>
2021-12-28 15:09:37 +0200

    complete-js-course - 39 Introduction to Arrays

--- js/complete-js-course/02-Fundamentals-Part-2/script.js
+++ js/complete-js-course/02-Fundamentals-Part-2/script.js
@@ -184,6 +184,60 @@ GOOD LUCK 😀
 // checkWinner(scoreDolphins, scoreKoalas);
 
 
+// ============ 39 Introduction to Arrays ============
+
+// const friend1 = 'Michael';
+// const friend2 = 'Steven';
+// const friend3 = 'Peter';
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
+
+// const y = new Array(1991, 1984, 2008, 2020);
+
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]); // Peter
+
+// friends[2] = 'Jay';
+// console.log(friends); // ['Michael', 'Steven', 'Jay']
+// // friends = ['Bob', 'Alice']; // Error
+
+// const jonas = ['Jonas', 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+// console.log(jonas);
+
+// // Exercise
+// const calcAge = function (birthYeah) {
+//     return 2037 - birthYeah;
+// }
+
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// // console.log(calcAge(years)); // errors
+// const age1 = (calcAge(years[0]));
+// const age2 = (calcAge(years[1]));
+// const age3 = (calcAge(years[years.length - 1]));
+
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+// console.log('ages', ages);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
