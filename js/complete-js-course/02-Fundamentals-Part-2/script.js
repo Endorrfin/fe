d2cccb89696412cfbc95ccb1525fc61c7821e0db
Krupka <krupka.ua@gmail.com>
2021-12-28 12:33:30 +0200

    complete-js-course - 35 Arrow Functions

--- js/complete-js-course/02-Fundamentals-Part-2/script.js
+++ js/complete-js-course/02-Fundamentals-Part-2/script.js
@@ -62,6 +62,30 @@
 
 
 
+// ============ 35 ARROW FUNCTION  ============
+
+// // Arrow function
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+//
+// const yearsUntilRetirement = birthYear => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     return retirement;
+// }
+//
+// console.log(yearsUntilRetirement(1991));
+//
+//
+// const yearsUntilRetirementParameters = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     return `${firstName} retires in ${retirement}`;
+// }
+//
+// console.log(yearsUntilRetirementParameters(1991, 'Jonas'));
+// console.log(yearsUntilRetirementParameters(1980, 'Bob'));
 
 
 
