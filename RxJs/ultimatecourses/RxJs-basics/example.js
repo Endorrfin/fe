c06f7118a10d08ea0509827582a8928e4e7d2ccd
Krupka <krupka.ua@gmail.com>
2022-01-02 10:18:02 +0200

    RxJS Basics - 3 Create Your First Observable

--- RxJs/ultimatecourses/RxJs-basics/example.js
+++ RxJs/ultimatecourses/RxJs-basics/example.js
@@ -1,24 +1,28 @@
-import { Observable } from 'rxjs';
+// import { Observable } from 'rxjs';
+//
+// const observer = {
+//     next: value => console.log('next', value),
+//     error: error => console.log('error', error),
+//     complete: () => console.log('complete!')
+// };
+//
+//
+// const observable = new Observable(subscriber => {
+//     subscriber.next('Hello');
+//     subscriber.next('World');
+//     subscriber.complete();
+//     subscriber.next('Hello');
+//     subscriber.next('World');
+// })
+//
+// observable.subscribe(observer);
+// // observer here
+//
 
-const observer = {
-    next: value => console.log('next', value),
-    error: error => console.log('error', error),
-    complete: () => console.log('complete!')
-};
 
-
-const observable = new Observable(subscriber => {
-    subscriber.next('Hello');
-    subscriber.next('World');
-    subscriber.complete();
-    subscriber.next('Hello');
-    subscriber.next('World');
+const observable = Rx.Observable.create(observer => {
+    observer.next('Hello')
+    observer.next('world')
 })
 
-observable.subscribe(observer);
-    // observer here
-
-
-
-
-
+observable.subscribe(val => print(val))
