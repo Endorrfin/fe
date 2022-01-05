bbbc79850e07a8117bcae5337668995e4f66ebec
Krupka <krupka.ua@gmail.com>
2022-01-05 13:56:34 +0200

    RxJS Basics - initial commit

---
+++ RxJs/ultimatecourses/RxJs-basics/script.js
@@ -0,0 +1,8 @@
+import { of } from 'rxjs';
+
+/*
+ * Any code samples you want to play with can go in this file.
+ * Updates will trigger a live reload on http://localhost:1234/
+ * after running npm start.
+ */
+of('Hello', 'RxJs-Basics').subscribe(console.log);
