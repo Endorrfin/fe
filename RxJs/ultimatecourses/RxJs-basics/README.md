ae92175e307afc1f020ab68affe2e7e8d549ca52
Krupka <krupka.ua@gmail.com>
2022-01-06 14:01:23 +0200

    RxJS Basics - 1 The Missing Introduction to RxJS

--- RxJs/ultimatecourses/RxJs-basics/README.md
+++
@@ -1,8 +0,0 @@
-RxJS Basics
-
-Getting Started
-Server running at http://localhost:55563
-cd rxjs-basics
-npm install
-npm start
-After running npm start you can navigate to the development server running on http://localhost:1234. Any changes made to the sample.js file will trigger an automatic reload.
--- RxJs/ultimatecourses/RxJs-basics/script.js
+++
@@ -1,9 +0,0 @@
-import { Observable, fromEvent, of } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
- */
-of('Hello', 'RxJs-Basics').subscribe(console.log);
