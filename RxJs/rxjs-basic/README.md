5cc00288b277b7635002576e3ef375dbff766818
Krupka <krupka.ua@gmail.com>
2022-01-07 11:37:57 +0200

    3 Create Your First Observable

---
+++ RxJs/rxjs-basic/README.md
@@ -0,0 +1,9 @@
+RxJS Basic
+Basic starter project for RxJS, helper for RxJS Basics course on Ultimate Courses.
+
+Getting Started
+cd rxjs-basic
+npm install
+npm start
+After running npm start you can navigate to the development server running on http://localhost:1234. 
+Any changes made to the sample.js file will trigger an automatic reload.
--- RxJs/rxjs-basic/script.js
+++ RxJs/rxjs-basic/script.js
@@ -1,9 +1,19 @@
 import { Observable, fromEvent, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
- */
-of('Hello', 'RxJs-Basic!').subscribe(console.log);
+
+const observer = {
+    next: value => console.log('next', value),
+    error: error => console.log('error', value),
+    complete: () => console.log('complete!')
+}
+
+const observable = new Observable(subscriber => {
+    subscriber.next('Hello');
+    subscriber.next('Wold');
+    subscriber.complete();
+    subscriber.next('Hello');
+    subscriber.next('Wold');
+})
+
+observable.subscribe(observer)
