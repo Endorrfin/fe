29ef2cfeb8fc1587a18910f402ac86a4252ade22
Krupka <krupka.ua@gmail.com>
2022-01-07 15:12:32 +0200

    complete-js-course - 46 Iteration The for Loop

--- js/complete-js-course/02-Fundamentals-Part-2/script.js
+++ js/complete-js-course/02-Fundamentals-Part-2/script.js
@@ -446,6 +446,26 @@ GOOD LUCK 😀
 // }
 
 
+// ============ 46 Iteration The for Loop ============
+
+// console.log('Lifting weights repetition 1 🏋️‍♀️');
+// console.log('Lifting weights repetition 2 🏋️‍♀️');
+// console.log('Lifting weights repetition 3 🏋️‍♀️');
+// console.log('Lifting weights repetition 4 🏋️‍♀️');
+// console.log('Lifting weights repetition 5 🏋️‍♀️');
+// console.log('Lifting weights repetition 6 🏋️‍♀️');
+// console.log('Lifting weights repetition 7 🏋️‍♀️');
+// console.log('Lifting weights repetition 8 🏋️‍♀️');
+// console.log('Lifting weights repetition 9 🏋️‍♀️');
+// console.log('Lifting weights repetition 10 🏋️‍♀️');
+
+// for loop keeps running while condition is TRUE
+// for(let rep = 1; rep <= 10;  rep = rep + 1) {
+//     console.log(`Lifting weights repetition ${rep} 🏋️‍♀ `);
+// }
+
+
+
 
 
 
