65a613a7711974380945c74c8202349bcbb18c41
Krupka <krupka.ua@gmail.com>
2022-01-07 11:22:58 +0200

    1 The Missing Introduction to RxJS

---
+++ RxJs/rxjs-basic/script.js
@@ -0,0 +1,9 @@
+import { Observable, fromEvent, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+/*
+ * Any code samples you want to play with can go in this file.
+ * Updates will trigger a live reload on http://localhost:1234/
+ * after running npm start.
+ */
+of('Hello', 'RxJs-Basic!').subscribe(console.log);
