4390245d1f6c5051e52b3f96b7a68040aba5be44
Krupka <krupka.ua@gmail.com>
2022-01-07 13:57:24 +0200

    complete-js-course - 44 Object Methods

--- js/complete-js-course/02-Fundamentals-Part-2/script.js
+++ js/complete-js-course/02-Fundamentals-Part-2/script.js
@@ -356,6 +356,47 @@ GOOD LUCK 😀
 // console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
 
 
+// ============ 44 Object Methods ============
+
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     birthYear: 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven'],
+//     hasDriversLicense: true,
+//
+//     // calcAge: function (birthYear) {
+//     //     return 2037 - birthYear;
+//     // },
+//
+//     // calcAge: function () {
+//     //     console.log(this); // jonas Object
+//     //     // return 2037 - jonas.birthYear; // Don't repeat yourself
+//     //     return 2037 - this.birthYear;
+//     // },
+//
+//     calcAge: function (birthYear) {
+//         this.age = 2037 - this.birthYear;
+//         return this.age;
+//     },
+//
+//     getSummary: function () {
+//         return `${this.firstName} is a ${this.calcAge()} -year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+//     }
+// };
+//
+// // console.log(jonas.calcAge(1991));
+// // console.log(jonas[' '](1991));
+//
+// console.log(jonas.calcAge());
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age);
+//
+// // Challenge
+// // "Jonas is a 46-year old teacher, and he has a driver's license"
+// console.log(jonas.getSummary());
 
 
 
