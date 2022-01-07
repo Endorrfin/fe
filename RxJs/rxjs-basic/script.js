13b5bebd54d701965e210ae1f5746ab66d2a1e90
Krupka <krupka.ua@gmail.com>
2022-01-07 11:50:32 +0200

    5 Deliver Values Asynchronously with Observables

--- RxJs/rxjs-basic/script.js
+++ RxJs/rxjs-basic/script.js
@@ -4,18 +4,31 @@ import { map } from 'rxjs/operators';
 
 const observer = {
     next: value => console.log('next', value),
-    // error: error => console.log('error', value),
-    // complete: () => console.log('complete!')
+    error: error => console.log('error', value),
+    complete: () => console.log('complete!')
 }
 
-const observable = new Observable(subscriber => {
-    subscriber.next('Hello');
-    subscriber.next('Wold');
-    subscriber.complete();
-})
+// const observable = new Observable(subscriber => {
+//     subscriber.next('Hello');
+//     subscriber.next('Wold');
+//     subscriber.complete();
+// })
 
-observable.subscribe(
-    value => console.log('next', value),
-    error => console.log('error', error),
-    () => console.log('complete')
-);
+const observable = new Observable(subscriber => {
+    let count = 0;
+
+    const id = setInterval(() => {
+        subscriber.next(count);
+        subscriber.complete();
+        count += 1;
+    }, 1000);
+
+    return () => {
+        console.log('called');
+        clearInterval(id);
+    }
+});
+
+console.log('before');
+observable.subscribe(observer);
+console.log('after');
