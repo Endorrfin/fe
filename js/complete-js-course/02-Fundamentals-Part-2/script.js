5c3e2d202332b6e8432787b1d95305d6497e6043
Krupka <krupka.ua@gmail.com>
2022-01-10 11:38:23 +0200

    complete-js-course - 48 Looping Backwards and Loops in Loops

--- js/complete-js-course/02-Fundamentals-Part-2/script.js
+++ js/complete-js-course/02-Fundamentals-Part-2/script.js
@@ -526,6 +526,42 @@ GOOD LUCK 😀
 
 
 
+// ============ 48 Looping Backwards and Loops in Loops ============
+
+// const jonas = [
+//     'Jonas',
+//     'Shmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven'],
+//     true
+// ];
+//
+// // 0, 1, ..., 4
+// // 4, 3, ..., 0
+//
+// for (let i = jonas.length - 1; i >= 0; i--) {
+//     console.log(i, jonas[i]);
+// }
+//
+// for (let exercise = 1; exercise < 4; exercise++) {
+//     console.log(`------- STARTING EXERSISE ${exercise}`);
+//
+//     // for (let rep = 1; rep < 6; rep++) {
+//     //     console.log(`Lifting weight repetition ${rep} 🏋️‍♀`)
+//     // }
+//
+//     for (let rep = 1; rep < 6; rep++) {
+//         console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀`)
+//     }
+// }
+
+
+
+
+
+
+
 
 
 
