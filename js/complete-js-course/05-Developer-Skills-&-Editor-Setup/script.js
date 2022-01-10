b55f25265d58865a88d8c51a460024c753482a5c
Krupka <krupka.ua@gmail.com>
2022-01-10 18:41:32 +0200

    complete-js-course - 55 Setting up Prettier and VS Code

---
+++ js/complete-js-course/05-Developer-Skills-&-Editor-Setup/script.js
@@ -0,0 +1,36 @@
+
+// Remember, we're gonna use strict mode in all
+"use strict"
+
+const x = "23";
+if (x === 23) console.log(23);
+
+const calcAge = (birthYear) => 2037 - birthYear;
+
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
