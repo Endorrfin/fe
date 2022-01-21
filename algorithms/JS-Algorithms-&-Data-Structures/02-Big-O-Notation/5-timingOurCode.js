b70dca971658d354f35796f29a89021b1d9a20fc
Krupka <krupka.ua@gmail.com>
2022-01-21 16:47:21 +0200

    JS Algorithms & Data Structures | Big O Notation | 5 Timing our code

--- algorithms/JS-Algorithms-&-Data-Structures/02-Big-O-Notation/5-timingOurCode.js
+++ algorithms/JS-Algorithms-&-Data-Structures/02-Big-O-Notation/5-timingOurCode.js
@@ -11,7 +11,7 @@ function addUpTo1(n) {
 let time1 = Date.now();
 console.log('Option I', addUpTo1(1000000000));
 let time2 = Date.now();
-console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`);
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`); // Time Elapsed: 2.257 seconds.
 
 // ======= IMPLEMENTATION #2 =======
 function addUpTo2(n) {
@@ -21,4 +21,4 @@ function addUpTo2(n) {
 let time3 = Date.now();
 console.log('Option II', addUpTo2(1000000000));
 let time4 = Date.now();
-console.log(`Time Elapsed: ${(time4 - time3) / 1000} seconds.`);
+console.log(`Time Elapsed: ${(time4 - time3) / 1000} seconds.`); // Time Elapsed: 0.001 seconds.
