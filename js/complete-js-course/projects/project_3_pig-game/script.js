b79dc90ec713d7f2b8186c2ed32d6f21a5773d82
Krupka <krupka.ua@gmail.com>
2022-01-26 09:26:06 +0200

    PROJECT #3 Pig game - Resetting the Game

--- js/complete-js-course/projects/project_3_pig-game/script.js
+++ js/complete-js-course/projects/project_3_pig-game/script.js
@@ -5,8 +5,7 @@ const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
 const score0El = document.querySelector('#score--0');
-// const score1El = document.getElementById('score--1');
-const score1El = document.querySelector('#score--1');
+const score1El = document.getElementById('score--1');
 
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
@@ -16,16 +15,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
 
 // STARTING CONDITIONS
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+
+init();
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -66,7 +77,7 @@ btnHold.addEventListener('click', function() {
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 9) {
+        if (scores[activePlayer] >= 100) {
             // Finish the game
             playing = false;
             diceEl.classList.add('hidden');
@@ -79,5 +90,8 @@ btnHold.addEventListener('click', function() {
         }
     }
 });
+
+
+btnNew.addEventListener('click', init);
 
 
