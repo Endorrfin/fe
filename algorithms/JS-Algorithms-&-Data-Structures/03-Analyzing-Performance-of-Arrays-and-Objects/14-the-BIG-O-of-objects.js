96a2be3e1168f298890ad63beadc7b150e043163
Krupka <krupka.ua@gmail.com>
2022-01-26 11:35:56 +0200

    JS Algorithms & Data Structures | Big O Notation | The BIG O of Objects

---
+++ algorithms/JS-Algorithms-&-Data-Structures/03-Analyzing-Performance-of-Arrays-and-Objects/14-the-BIG-O-of-objects.js
@@ -0,0 +1,18 @@
+
+// ======= IMPLEMENTATION #1 - The BIG O of Objects =======
+const instructor = {
+    firstName: "Kelly",
+    isInstructor: true,
+    favoriteNumbers: [1, 2, 3, 4]
+}
+
+// console.log(Object.keys(instructor)); // [ 'firstName', 'isInstructor', 'favoriteNumbers' ]
+// console.log(Object.values(instructor)); // [ 'Kelly', true, [ 1, 2, 3, 4 ] ]
+// console.log(Object.entries(instructor)); // [ 'firstName', 'Kelly' ], [ 'isInstructor', true ], [ 'favoriteNumbers', [ 1, 2, 3, 4 ] ]
+//
+// console.log(instructor.hasOwnProperty('firstName')); // true
+// console.log(instructor.hasOwnProperty('isInstructor')); // true
+// console.log(instructor.hasOwnProperty('favoriteNumbers')); // true
+
+
+
