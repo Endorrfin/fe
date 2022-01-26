95e031ed0cd884cbdb953f06760fcf24ce7900fd
Krupka <krupka.ua@gmail.com>
2022-01-26 11:43:16 +0200

    JS Algorithms & Data Structures | Big O Notation | Big O of Array Operations

---
+++ algorithms/JS-Algorithms-&-Data-Structures/03-Analyzing-Performance-of-Arrays-and-Objects/15-when-arrays-show.js
@@ -0,0 +1,18 @@
+// ======= IMPLEMENTATION #1 - When Arrays show =======
+
+let names = ['Michael', 'Melissa', 'Andrea'];
+let values = [true, {}, [], 2, 'awesome'];
+
+/*
+Big O of Array Operations
+push - o(1);
+pop - o(1);
+shift - o(n);
+unshift - o(n);
+concat - o(n);
+slice - o(n);
+splice - o(n);
+sort - o(n);
+forEach / map / filter / reduce / etc. - o(n);
+ */
+
