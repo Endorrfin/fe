606ad5241acf59f1d3bb9291d54e0ecfc38ed340
Krupka <krupka.ua@gmail.com>
2022-01-31 00:47:40 +0200

    complete-js-course | 08 How JavaScript Works Behind the Scenes - 95 Hoisting and TDZ in Practice

--- js/complete-js-course/08-How-JavaScript-Works-Behind-the-Scenes/script.js
+++ js/complete-js-course/08-How-JavaScript-Works-Behind-the-Scenes/script.js
@@ -2,57 +2,120 @@
 
 // ============ 93 Scoping in Practice ============
 
-function calcAge(birthYear) {
-    const age = 2037 - birthYear;
-
-    function printAge() {
-        let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-        console.log(output);
-
-        if (birthYear >= 1981 && birthYear <= 1996) {
-            var millenial = true;
-            // Creating NEW variable with same name as outer scope's variable
-            const firstName = 'Steven';
-
-            // Reassigning outer scope's variable
-            output = 'NEW OUTPUT!';
-
-            const str = `Oh, and you're a millenial, ${firstName}`;
-
-            console.log(str);
-            function add(a, b) {
-                return a + b;
-
-            }
-        }
-        // console.log(str);
-        console.log(millenial);
-        // console.log(add(2, 3));
-        console.log(output);
-    }
-    printAge();
-    return age;
-}
-
-const firstName = 'Jonas';
-calcAge(1991);
-
-// console.log(age);
-// printAge();
+// function calcAge(birthYear) {
+//     const age = 2037 - birthYear;
+//
+//     function printAge() {
+//         let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//         console.log(output);
+//
+//         if (birthYear >= 1981 && birthYear <= 1996) {
+//             var millenial = true;
+//             // Creating NEW variable with same name as outer scope's variable
+//             const firstName = 'Steven';
+//
+//             // Reassigning outer scope's variable
+//             output = 'NEW OUTPUT!';
+//
+//             const str = `Oh, and you're a millenial, ${firstName}`;
+//
+//             console.log(str);
+//             function add(a, b) {
+//                 return a + b;
+//
+//             }
+//         }
+//         // console.log(str);
+//         console.log(millenial);
+//         // console.log(add(2, 3));
+//         console.log(output);
+//     }
+//     printAge();
+//     return age;
+// }
+//
+// const firstName = 'Jonas';
+// calcAge(1991);
+//
+// // console.log(age);
+// // printAge();
 
 
 
 // ============ 94 Variable Environment Hoisting and The TDZ ============
 
-const myName = 'Jonas'
+// const myName = 'Jonas'
+//
+// if (myName === 'Jonas') {
+//     console.log(`Jonas is a ${job}`);
+//     const age = 2037 - 1989;
+//     console.log(age);
+//     const job = 'teacher';
+//     console.log(x);
+// }
 
-if (myName === 'Jonas') {
-    console.log(`Jonas is a ${job}`);
-    const age = 2037 - 1989;
-    console.log(age);
-    const job = 'teacher';
-    console.log(x);
-}
+
+// ============ 95 Hoisting and TDZ in Practice ============
+
+// // Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
+//
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+//
+//
+// // Functions
+//
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// // console.log(addArrow(2, 3));
+//
+// // console.log(addExprVar(2, 3));
+// // console.log(addArrowVar(2, 3));
+//
+// function addDecl(a, b) {
+//     return a + b;
+// };
+//
+// const addExpr = function(a, b) {
+//     return a + b;
+// };
+//
+// const addArrow = (a, b) => a + b;
+//
+//
+//
+// var addExprVar = function(a, b) {
+//     return a + b;
+// };
+//
+// var addArrowVar = (a, b) => a + b;
+//
+//
+// // Example
+// if (!numProducts) deleteShoppingCart();
+//
+// var numProducts = 10;
+//
+// function deleteShoppingCart() {
+//     console.log('All products deleted');
+// }
+//
+//
+// var x = 1; // window in console
+// let y = 2;
+// const z = 3;
+//
+// console.log(x === window.x); // true
+// console.log(y === window.y); // false
+// console.log(z === window.z); // false
+
+
+
+
 
 
 
