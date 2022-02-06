491725c7dfe546def6066231a3b12e894ba5a1ff
Krupka <krupka.ua@gmail.com>
2022-02-06 04:53:57 +0200

    complete-js-course | How JavaScript works behind the scenes | 98 Regular Functions vs. Arrow Functions

--- js/complete-js-course/08-How-JavaScript-Works-Behind-the-Scenes/script.js
+++ js/complete-js-course/08-How-JavaScript-Works-Behind-the-Scenes/script.js
@@ -158,6 +158,78 @@
 
 
 
+// ============ 98 Regular Functions vs. Arrow Functions ============
+//
+// var firstName = 'Matilda';
+//
+// const jonas = {
+//     firstName: 'Jonas',
+//     year: 1991,
+//     calcAge: function () {
+//         console.log(this);
+//         console.log(2037 - this.year);
+//
+//
+//     //  // Error
+//     //     const isMillenial = function() {
+//     //         console.log(this);
+//     //         // console.log(this.year >= 1981 && this.year <= 1996); // Error
+//     //     };
+//     //     isMillenial(); // undefined
+//     // },
+//
+//         // Solution 1
+//     //     const self = this; // self of that
+//     //     const isMillenial = function() {
+//     //         console.log(this);
+//     //         console.log(self.year >= 1981 && self.year <= 1996); // object(jonas)
+//     //     };
+//     //     isMillenial(); // true
+//     // },
+//
+//         // Solution 2
+//         const isMillenial = () => {
+//             console.log(this);
+//             console.log(this.year >= 1981 && this.year <= 1996); // object(jonas)
+//         };
+//         isMillenial(); // true
+//     },
+//
+//     // Arrow Function
+//     // greet: () => {
+//     //     console.log(this);
+//     //     console.log(`Hey ${this.firstName}`)
+//     // },
+//
+//     // Regular Function
+//     greet: function () {
+//         console.log(this);
+//         console.log(`Hey ${this.firstName}`)
+//     },
+// };
+//
+// jonas.greet();
+// jonas.calcAge();
+//
+//
+// // arguments keyword
+// const addExpr = function (a, b) {
+//     console.log(arguments);
+//     return a + b;
+// };
+// addExpr(2, 5);
+// addExpr(2, 5, 8, 12);
+//
+//
+// var addArrow = (a, b) => {
+//     console.log(arguments);
+//     return a + b;
+// }
+//
+// addArrow(2, 4, 8); // arguments is not defined
+//
+
+
 
 
 
