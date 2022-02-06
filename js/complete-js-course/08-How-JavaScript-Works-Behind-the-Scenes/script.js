1e23694720b2cefbca5ab156f10701425212e46a
Krupka <krupka.ua@gmail.com>
2022-02-06 05:15:22 +0200

    complete-js-course | How JavaScript works behind the scenes | 99 Primitives vs. Objects (Primitives vs. Reference Types)

--- js/complete-js-course/08-How-JavaScript-Works-Behind-the-Scenes/script.js
+++ js/complete-js-course/08-How-JavaScript-Works-Behind-the-Scenes/script.js
@@ -231,6 +231,24 @@
 
 
 
+// ============ 99 Primitives vs. Objects (Primitives vs. Reference Types) ============
+
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(age); // 31
+// console.log(oldAge); // 30
+//
+//
+// const me = {
+//     name: 'Jonas',
+//     age: 30,
+// };
+//
+// const friend = me;
+// friend.age = 27;
+// console.log('Friend: ', friend); // Friend:  {name: 'Jonas', age: 27}
+// console.log('Me', me); // Me {name: 'Jonas', age: 27}
 
 
 
