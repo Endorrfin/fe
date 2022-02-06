f43fb93bced740a8bfadd62ef62151966a15fa19
Krupka <krupka.ua@gmail.com>
2022-02-06 05:47:13 +0200

    complete-js-course | How JavaScript works behind the scenes | 100 Primitives vs. Objects if Practice

--- js/complete-js-course/08-How-JavaScript-Works-Behind-the-Scenes/script.js
+++ js/complete-js-course/08-How-JavaScript-Works-Behind-the-Scenes/script.js
@@ -252,6 +252,47 @@
 
 
 
+// ============ 100 Primitives vs. Objects if Practice ============
+
+// // Primitives types
+// let lastName = 'Williams';
+// let oldLastName = lastName;
+// lastName = 'Davis';
+// console.log(lastName, oldLastName);
+//
+//
+// // Reference types
+// const jessica = {
+//     firstName: 'Jessica',
+//     lastName: 'Williams',
+//     age: 27,
+// };
+//
+// const marriedJessica = jessica;
+// marriedJessica.lastName = 'Davis';
+// console.log('Before marriage:', jessica);
+// console.log('After marriage:', marriedJessica);
+//
+//
+// // Copying objects
+// const jessica2 = {
+//     firstName: 'Jessica',
+//     lastName: 'Williams',
+//     age: 27,
+//     family: ['Alice', 'Bob'],
+// };
+//
+//
+// const jessicaCopy = Object.assign({}, jessica2);
+// jessicaCopy.lastName = 'Davis';
+//
+// jessicaCopy.family.push('Mary');
+// jessicaCopy.family.push('John');
+//
+// console.log('Before marriage:', jessica2); // {firstName: 'Jessica', lastName: 'Williams', age: 27}
+// console.log('After marriage:', jessicaCopy); // {firstName: 'Jessica', lastName: 'Davis', age: 27}
+
+
 
 
 
