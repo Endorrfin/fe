fdad7714c489f837fe2701bf85b3bc2898e740ee
Krupka <krupka.ua@gmail.com>
2022-02-11 17:11:55 +0200

    complete-js-course | Data structures and Modern Operators | 118 Maps Iteration

--- js/complete-js-course/09-Data-structures-Modern-operators-and-Strings/script.js
+++ js/complete-js-course/09-Data-structures-Modern-operators-and-Strings/script.js
@@ -628,45 +628,87 @@ GOOD LUCK 😀
 // ==========================================================
 //            117 Maps Fundamentals
 // ==========================================================
+//
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+//
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed :');
+//
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+//
+// // const time = 21;
+// const time = 8;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+//
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// console.log(rest);
+// console.log(rest.size);
+// // rest.clear();
+// console.log(rest.size);
+//
+// rest.set([1, 2], 'Test');
+// console.log(rest);
+//
+// console.log(rest.get([1, 2])); // undefined
+//
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// console.log(rest.get(arr));
+//
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
 
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-console.log(rest.set(2, 'Lisbon, Portugal'));
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open :D')
-  .set(false, 'We are closed :');
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// ==========================================================
+//            118 Maps Iteration
+// ==========================================================
 
-// const time = 21;
-const time = 8;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'Javascript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try again'],
+]);
 
-console.log(rest.has('categories'));
-rest.delete(2);
-console.log(rest);
-console.log(rest.size);
-// rest.clear();
-console.log(rest.size);
+console.log(question);
 
-rest.set([1, 2], 'Test');
-console.log(rest);
+// Convert object to map
+console.log(Object.entries(openingHours));
 
-console.log(rest.get([1, 2])); // undefined
+const hoursMap = new Map(Object.entries(openingHours));
+console.log( hoursMap);
 
-const arr = [1, 2];
-rest.set(arr, 'Test');
-console.log(rest.get(arr));
+// Quiz app
+console.log( question.get('question'));
+for (const [key, value] of question) {
+  if (typeof  key === 'number') console.log(` Answer ${key}: ${value}`);
+}
 
-rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest);
+// const answer = Number(prompt('Your answer'));
+const answer = 3
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer))
+
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log(...question.keys());
+console.log(...question.values());
 
 
 
