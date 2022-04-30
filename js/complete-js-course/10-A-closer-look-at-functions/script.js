f8d5828d0b9ca2adb519146a80231737ced04ec7
VasylKrupka <VasylKrupka@coherentsolutions.com>
2022-04-30 23:54:55 +0300

    JS | A Closer Look at Functions | 128 Default Parameters

--- js/complete-js-course/10-A-closer-look-at-functions/script.js
+++ js/complete-js-course/10-A-closer-look-at-functions/script.js
@@ -4,7 +4,32 @@
 //            128 Default Parameters
 // ==========================================================
 
+const bookings = [];
+const createBooking = function(
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
 
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH124', 2, 800);
+createBooking('LH125', 3);
+createBooking('LH126', 5);
+
+createBooking('LH127', undefined, 1000);
 
 
 
