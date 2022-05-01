4cf7cccab9faa53b3e0a455f8dea0e04e4d5bf6a
VasylKrupka <VasylKrupka@coherentsolutions.com>
2022-05-01 06:53:43 +0300

    JS | A Closer Look at Functions | 130 First-Class and Higher-Order Functions

--- js/complete-js-course/10-A-closer-look-at-functions/script.js
+++ js/complete-js-course/10-A-closer-look-at-functions/script.js
@@ -36,37 +36,37 @@
 //    129 How Passing Arguments Works: Value vs. Reference
 // ==========================================================
 
-const flight = 'LH234';
-const jonas = {
-  name: 'Jonas Schmedtmann',
-  passport: 24739479284
-};
-
-const checkIn = function(flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
-
-  if (passenger.passport === 24739479284) {
-    alert('Checked in');
-  } else {
-    alert('Wrong passport!');
-  }
-};
-
-// checkIn(flight, jonas);
-// console.log(flight);
-// console.log(jonas);
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 24739479284
+// };
 //
-// // Is the same as doing...
-// const flightNum = flight;
-// const passenger = jonas;
-
-const newPassport = function(person) {
-  person.passport = Math.trunc(Math.random() * 100000000000);
-};
-
-newPassport(jonas);
-checkIn(flight, jonas);
+// const checkIn = function(flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+//
+//   if (passenger.passport === 24739479284) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
+//
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+// //
+// // // Is the same as doing...
+// // const flightNum = flight;
+// // const passenger = jonas;
+//
+// const newPassport = function(person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000);
+// };
+//
+// newPassport(jonas);
+// checkIn(flight, jonas);
 
 
 // ==========================================================
