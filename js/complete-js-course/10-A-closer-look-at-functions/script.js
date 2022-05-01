61d67a9838066b05ec80b2d86f0f56bafcf79b3d
VasylKrupka <VasylKrupka@coherentsolutions.com>
2022-05-01 06:35:01 +0300

    JS | A Closer Look at Functions | 129 How Passing Arguments Works: Value vs. Reference

--- js/complete-js-course/10-A-closer-look-at-functions/script.js
+++ js/complete-js-course/10-A-closer-look-at-functions/script.js
@@ -4,32 +4,76 @@
 //            128 Default Parameters
 // ==========================================================
 
-const bookings = [];
-const createBooking = function(
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
+// const bookings = [];
+// const createBooking = function(
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//
+//   // ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
+//
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+//
+// createBooking('LH123');
+// createBooking('LH124', 2, 800);
+// createBooking('LH125', 3);
+// createBooking('LH126', 5);
+//
+// createBooking('LH127', undefined, 1000);
 
-  // ES5
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price
-  };
-  console.log(booking);
-  bookings.push(booking);
+// ==========================================================
+//    129 How Passing Arguments Works: Value vs. Reference
+// ==========================================================
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284
 };
 
-createBooking('LH123');
-createBooking('LH124', 2, 800);
-createBooking('LH125', 3);
-createBooking('LH126', 5);
+const checkIn = function(flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
 
-createBooking('LH127', undefined, 1000);
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+//
+// // Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function(person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+
+
+
+
+
+
 
 
 
