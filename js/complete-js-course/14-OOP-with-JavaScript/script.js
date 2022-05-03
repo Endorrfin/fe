09e90c374413c9d0cb2c959b9399981fac14bf53
VasylKrupka <VasylKrupka@coherentsolutions.com>
2022-05-03 18:15:41 +0300

    JS | OOP with JavaScript | 206 What is Object-Oriented Programming

---
+++ js/complete-js-course/14-OOP-with-JavaScript/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// ==========================================================
+//            206 What is Object-Oriented Programming?
+// ==========================================================
+
+/*
+* 1. Abstraction
+* 2. Encapsulation
+*    Encapsulation: Keeping properties and methods private inside the class, so they are not accessible from
+*  outside the class. Some methods can be exposed as a public interface (API)
+* 3. Inheritance
+* 4. Polymorphism
+*    Polymorphism: A child class can overwrite a method it inherited from a paren class [it's more complex that that, but enough for our purposes].
+*
+*
+* */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
