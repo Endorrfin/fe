e39840b4d80cff0922ceded26fa9e75ca23f5405
VasylKrupka <VasylKrupka@coherentsolutions.com>
2022-10-01 23:57:51 +0300

    Synchronous & Asynchronous examples

--- interview/js/topic/js/tasks/eventLoop/eventLoop.js
+++ interview/js/topic/js/tasks/eventLoop/eventLoop.js
@@ -25,3 +25,44 @@
 // console.log(7);
 
 // Result - 1, 3, 5, 7, 4, 6, 2
+
+
+// Exercise I -  task
+// const third = (str => str + "Script.");
+// const second = () => third("Java ");
+//
+// const First = (() => {
+//     let name = second();
+//     console.log("My name is " + name);
+// })
+//
+// First ();
+
+
+// Output - Hello, I am JavaScript.
+// послідовність виконання - third(), second(), First().
+// Працюватиме лише callStack, без залучення чого-небудь ще.
+
+
+// Exercise II Asynchronous task
+// console.log('Hello,');
+//
+// setTimeout(function(Synchronous) {
+//     // This will execute in future after 5 seconds
+//     console.log('cool programming language')
+// }, 5000);
+//
+// setTimeout(function() {
+//     // This will execute in future after 3 seconds
+//     console.log('JavaScript - ');
+// }, 3000);
+// console.log("I am");
+
+
+// Output - Hello, I am JavaScript - cool programming language
+
+
+
+
+
+
