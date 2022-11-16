3c45153b061abcd9c96a7bfb88eea7eb1fa6442b
VasylKrupka <VasylKrupka@coherentsolutions.com>
2022-11-16 13:00:51 +0200

    Interview | TypeScript | Types

--- interview/ts/unit-01-basicTypes/app.ts
+++ interview/ts/unit-01-basicTypes/app.ts
@@ -28,23 +28,23 @@ enum QuestionStatus {
     Draft = 'draft',
     Deleted = 'deleted',
 }
-async function getFlags(req: {
-    topicId: number;
-    status?: QuestionStatus
-}): Promise<{
-    question: string;
-    answer: string;
-    tags: string[];
-    likes: number;
-    status: QuestionStatus;
-}[]> {
-    const res = await fetch('/fags', {
-        method: 'POST',
-        body: JSON.stringify(req)
-    });
-    const data = await res.json();
-    return data;
-}
+// async function getFlags(req: {
+//     topicId: number;
+//     status?: QuestionStatus
+// }): Promise<{
+//     question: string;
+//     answer: string;
+//     tags: string[];
+//     likes: number;
+//     status: QuestionStatus;
+// }[]> {
+//     const res = await fetch('/fags', {
+//         method: 'POST',
+//         body: JSON.stringify(req)
+//     });
+//     const data = await res.json();
+//     return data;
+// }
 
 
 
