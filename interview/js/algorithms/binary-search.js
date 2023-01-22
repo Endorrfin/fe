3b1ba3c07eb745215e68a9f21a4c83a92e5dc92a
VasylKrupka <VasylKrupka@coherentsolutions.com>
2023-01-22 12:38:07 +0200

    Algorithms and data structures | part I

---
+++ interview/js/algorithms/binary-search.js
@@ -0,0 +1,54 @@
+
+// ============ BINARY SEARCH ============
+const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+let count = 0;
+
+
+// ------------ Solution 1.1 - using cycle ------------
+function binarySearch(array, item) {
+  let start = 0;
+  let end = array.length;
+  let middle;
+  let found = false;
+  let position = -1
+
+  while(found === false && start <= end) {
+    count++;
+    middle = Math.floor((start + end) / 2);
+    if(array[middle] === item) {
+      found = true;
+      position = middle;
+      return position
+    }
+    if(item < array[middle]) {
+      end = middle - 1
+    } else {
+      start = middle + 1
+    }
+  }
+  return position;
+}
+
+// console.log( 'index of element = ', binarySearch(sortedArray, 27), 'count = ', count);
+
+
+
+
+// ------------ Solution 1.2 - using recursion ------------
+function recursiveBinarySearch(array, item, start, end) {
+  let middle = Math.floor((start + end) / 2);
+  count++;
+  if (item === array[middle]) {
+    return middle;
+  }
+
+  if(item < array[middle]) {
+    return recursiveBinarySearch(array, item, 0, middle - 1)
+  } else {
+    return recursiveBinarySearch(array, item, middle + 1, end)
+  }
+}
+
+// console.log( 'index of element = ', recursiveBinarySearch(sortedArray, 27, 0, sortedArray.length), 'count = ', count);
+// console.log('Value is: ', sortedArray[26]);
+
